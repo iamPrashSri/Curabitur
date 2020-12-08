a79cc1748da5fe6bc36e9a30efe53a1f47907bf4
Prashant Srivastava <prashantsrivastava.knowledge@gmail.com>
2020-12-08 23:49:19 +0530

    Initialize the Project

---
+++ curabitur-the-chat-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello Clever Programmer, Lets build Curabitur</h1>
+
+      {/* Header */}
+      {/* Sidebar */}
+      {/* React Router - Chat Screen */}
+    </div>
+  );
+}
+
+export default App;
